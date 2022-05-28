4c874a824a3f9794fc59011bcc08510c9dc58f96
nlom0218 <nlom0218@naver.com>
2022-05-28 13:17:16 +0900

    Custom App

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,10 @@
 import { useState } from "react";
-import NavBar from "../components/NavBar";
 
 export default function Home() {
   const [counter, setCounter] = useState(0);
   return (
     <div>
-      <NavBar />
       <h1>Hello</h1>
-      <style jsx>{`
-        h1 {
-          font-size: 52px;
-          color: blue;
-        }
-      `}</style>
     </div>
   );
 }
