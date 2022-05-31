28de85ba42c977b210c2f030c4c1e06bc6c8d5bc
nlom0218 <nlom0218@naver.com>
2022-05-31 23:58:46 +0900

    Fetching Data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,38 @@
-import Head from "next/head";
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "6725650d4790dfee5aff087618cb4a61";
+
+interface IMovie {
+  id: number;
+  original_title: string;
+}
+
 export default function Home() {
+  const [movies, setMovies] = useState<IMovie[] | []>([]);
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
+  console.log(movies);
+
   return (
     <div>
       <Seo title="Home" />
-      <h1>Hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies.map((moive) => {
+        return (
+          <div key={moive.id}>
+            <h4>{moive.original_title}</h4>
+          </div>
+        );
+      })}
     </div>
   );
 }
