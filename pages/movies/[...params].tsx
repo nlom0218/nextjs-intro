236f46da62574e4ed7d76ca8e73403f2333e874b
nlom0218 <nlom0218@naver.com>
2022-06-06 13:47:11 +0900

    Catch All #2

--- pages/movies/[...params].tsx
+++ pages/movies/[...params].tsx
@@ -1,16 +1,27 @@
 import { useRouter } from "next/router";
+import Seo from "../../components/Seo";
 
-export default function Detail() {
+interface IParams {
+  params: string[];
+}
+
+export default function Detail({ params }: IParams) {
   const router = useRouter();
-  console.log(router.query.params);
 
-  const [title, id]: [string | undefined, string | undefined] =
-    router.query.params || [];
-  console.log(title, id);
+  const [title, id] = params || [];
 
   return (
     <div>
+      <Seo title={title} />
       <h4>{title}</h4>
     </div>
   );
+}
+
+export async function getServerSideProps({ params: { params } }: IParams) {
+  return {
+    props: {
+      params,
+    },
+  };
 }
